3385de96f905f4e78c3da57171b2b9d34b37700a
Shigeki Yotsuzeki <yotsuzeki@gmail.com>
2026-03-29 00:18:21 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // ================================================================
 // OPERATION LOG Service Worker
-// Version  : 2.1.0
+// Version  : 2.1.1
 // Updated  : 2026-03-28
 // Author   : JQ3JQO / KyotoDR120
 // Changes  : 2.0.8 フェッチ戦略をネットワーク優先に変更
@@ -12,7 +12,7 @@
 // ============================================================
 // ★ Service Worker バージョン ★
 // HTMLのVERSIONと合わせて更新してください
-const SW_VERSION = '2.1.0';
+const SW_VERSION = '2.1.1';
 // ============================================================
 
 const CACHE_NAME = 'oplog-cache-v' + SW_VERSION;
